f8d8b79da392c177d66bab6a1f36f8e58ead2589
zhouzhiqin <kreviazukzhou@gmail.com>
2025-08-05 11:12:33 +0800

    谷歌商店测试包

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.running_tracker"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    // ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -37,6 +37,14 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    packagingOptions {
+        jniLibs {
+            // Allow symbol stripping for release builds
+            pickFirsts += "**/libc++_shared.so"
+            pickFirsts += "**/libjsc.so"
         }
     }
 }
